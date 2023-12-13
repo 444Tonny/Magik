a15bae4d9dcea154cad299f2151b557b17200e1a
444tonny <tonny.freelancing@gmail.com>
2023-12-13 13:04:44 -0500

    'SEO'

--- resources/views/client/header.blade.php
+++ resources/views/client/header.blade.php
@@ -2,14 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Descubre Magik Tienda, la tienda en línea donde puedes canjear tus puntos por increíbles recompensas, como la PS5, PC Gaming, auriculares Gaming, o incluso vivir una experiencia VLOG con el YouTuber Magik. ¡Explora nuestro universo de recompensas mágicas!">
+    <meta name="keywords" content="Magik, Tienda, Fortnite, Puntos, Contest, PS5">
+    <meta name="robots" content="index, follow">
     <meta name="author" content="Developer: Andriambololona Tonny, Designer: Andriambololona Tonny" />
     <link rel="stylesheet" href="{{ asset('css/client/layout.css?v2') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&family=Sora:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
-    <title>MAGIK</title>
+    <title>MAGIK TIENDA</title>
 </head>
 <body>
     <header>
